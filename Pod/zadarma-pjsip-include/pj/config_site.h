33b34df6f43feaa7fc1e6ad53d614268862860ca
Marina Zayceva <m.zayceva@sip-cloud.net>
2020-04-17 17:41:38 +0500

    0.6.0

--- Pod/zadarma-pjsip-include/pj/config_site.h
+++ Pod/zadarma-pjsip-include/pj/config_site.h
@@ -2,5 +2,6 @@
  #define PJ_HAS_IPV6 1
  #define PJSIP_AUTH_HEADER_CACHING 1
  #define PJ_AUTOCONF 1
-#define PJMEDIA_HAS_VIDEO 0
+ #define PJMEDIA_HAS_VIDEO 0
+ #define PJ_ENABLE_EXTRA_CHECK 0
 #include <pj/config_site_sample.h>
